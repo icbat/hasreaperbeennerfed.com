0038bd6fa431aee68885e0935a4e04f0e32d5fe9
Evan Cobb <icbat@pm.me>
2019-02-10 12:31:32 -0600

    looking for port variable from environment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const htmlSoup = require('html-soup')
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const MILLIS = 1000
 const SECONDS = 60
